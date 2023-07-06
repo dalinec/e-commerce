4b9471723c23f4abac2b89defa0325cc64a13d5b
dalinec <dalinec123@gmail.com>
2023-07-06 18:52:28 +0200

    add loading gif

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { Flex, Text } from '@chakra-ui/react';
+import Image from 'next/image';
+
+const Loading = () => {
+  return (
+    <Flex
+      minH='calc(100vh-150px)'
+      justify='center'
+      align='center'
+      flexDir='column'
+    >
+      <Image
+        src='/loading-cart.gif'
+        alt='loading cart'
+        width={200}
+        height={200}
+      />
+      <Text>Loading...</Text>
+    </Flex>
+  );
+};
+
+export default Loading;
